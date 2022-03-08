c00993004acb9e9b07c6c2b63c60672f51d2d4fb
Elias Frigård <ef222xf@student.lnu.se>
2022-03-08 13:26:53 +0200

    router setup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import './App.css'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 
-// Import components.
+// Import views and components.
 import Header from './components/Header'
 import Home from './views/Home'
 import Form from './views/Form'
 
 function App() {
   return (
-    <Router>
-      <div className='App'>
-        <Header />
-        <Route path='/home' component={Home} />
-        <Route path='/form' component={Form} />
-      </div>
-    </Router>
+    <div className='App'>
+      <Header />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/form' element={<Form />} />
+      </Routes>
+    </div>
   )
 }
 
