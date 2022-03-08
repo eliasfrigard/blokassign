41d137d91037534839a7d94357430e0345b22fa5
Elias Frigård <ef222xf@student.lnu.se>
2022-03-08 14:32:05 +0200

    basic icon button layout

--- src/components/IconButton.js
+++ src/components/IconButton.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
+import logo from '../assets/icons/apartment_house.svg'
+
 export default function IconButton() {
-  return <div>IconButton</div>
+  return (
+    <div className='IconButton'>
+      <img src={logo} alt='Blok Logo' />
+      <p>Apartment</p>
+    </div>
+  )
 }
