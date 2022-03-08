f51f1389be5a8f36ce6b67b53f5ccb8e186c89da
Elias Frigård <ef222xf@student.lnu.se>
2022-03-08 23:21:14 +0200

    enabled answer state change and save

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,21 +3,14 @@ import React, { useState } from 'react'
 import RangeInput from './RangeInput'
 import TextInput from './TextInput'
 
-export default function Question({ question, icon, minValue, maxValue }) {
-  // State variables.
-  const [answer, setAnswer] = useState(Math.round(maxValue / 2))
-
-  // Handle change of answer input.
-  const handleChange = (event) => {
-    const newValue = parseInt(event.target.value)
-
-    // Check if new value is within boundaries.
-    if (newValue < minValue || newValue > maxValue) return
-
-    // Set new value.
-    setAnswer(event.target.value)
-  }
-
+export default function Question({
+  question,
+  answer,
+  icon,
+  minValue,
+  maxValue,
+  onChange,
+}) {
   return (
     <div className='Question'>
       <h2>{question}</h2>
@@ -27,9 +20,9 @@ export default function Question({ question, icon, minValue, maxValue }) {
         answer={answer}
         min={minValue}
         max={maxValue}
-        onChange={handleChange}
+        onChange={onChange}
       />
-      <TextInput answer={answer} onChange={handleChange} />
+      <TextInput answer={answer} onChange={onChange} />
     </div>
   )
 }
--- src/views/Form.js
+++ src/views/Form.js
@@ -1,62 +1,112 @@
 import React, { useState } from 'react'
 
 import ProgressBar from '../components/ProgressBar'
-// import FormNav from '../components/FormNav'
 import Button from '../components/Button'
 import Question from '../components/Question'
 
-import size from '../assets/icons/size.svg'
-import rooms from '../assets/icons/rooms.svg'
-
 export default function Form() {
-  /* Mock Questions */
-  const questions = [
+  /* Hardcoded Mock Questions */
+  const [questions, setQuestions] = useState([
     {
       id: 1,
-      icon: size,
+      icon: './images/size.svg',
       question: 'How big is the apartment?',
       minValue: 1,
       maxValue: 200,
     },
     {
       id: 2,
-      icon: rooms,
+      icon: './images/rooms.svg',
       question: 'How many rooms does it have?',
       minValue: 1,
       maxValue: 10,
     },
-  ]
+  ])
 
   // State variable for the current question.
-  const [currentQuestion, setCurrentQuestion] = useState(0)
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
+
+  // Answer variable for shared state across components.
+  const [answer, setAnswer] = useState(1)
+
+  // HashMap containing all answers with question IDs.
+  const [answers, setAnswers] = useState(
+    new Map(questions.map((q) => [q.id, q.minValue]))
+  )
 
   // Variables for checking if there are next or previous questions.
-  const isFirst = questions[currentQuestion - 1] === undefined
-  const isLast = questions[currentQuestion + 1] === undefined
+  const isFirst = currentQuestionIndex === 0
+  const isLast = currentQuestionIndex === questions.length - 1
 
   // Progress variable used by the progress bar.
   // Equals the fraction of completed questions.
-  const progress = ((currentQuestion + 1) * 100) / questions.length
+  const progress = ((currentQuestionIndex + 1) * 100) / questions.length
 
-  // Changing the current question when using the navigation.
+  // Changes the current question when using navigation.
   const onClick = (event) => {
+    // Save the current question answer before changing view.
+    saveAnswers(currentQuestionIndex + 1, answer)
+
     if (event.target.value === 'prev') {
-      // Ignore if it is the first question.
-      if (!isFirst) setCurrentQuestion(currentQuestion - 1)
+      changeQuestion(currentQuestionIndex - 1)
     } else {
-      // Ignore if it is the last question.
-      if (!isLast) setCurrentQuestion(currentQuestion + 1)
+      changeQuestion(currentQuestionIndex + 1)
     }
+  }
+
+  // Handle change of answer input.
+  const onChange = (event) => {
+    const newValue = parseInt(event.target.value)
+
+    // Check if new value is within boundaries.
+    if (
+      newValue < questions[currentQuestionIndex].minValue ||
+      newValue > questions[currentQuestionIndex].maxValue
+    )
+      return
+
+    // Set new value.
+    setAnswer(event.target.value)
+  }
+
+  /**
+   * Saves a new answer to the collection by its ID.
+   */
+  const saveAnswers = (id, newAnswer) => {
+    // Copy answers map.
+    const answersCopy = new Map(answers)
+
+    // Update with the new answer.
+    answersCopy.set(id, newAnswer)
+
+    // Save the mutated hashmap.
+    setAnswers(answersCopy)
+  }
+
+  /**
+   * Changes the question view and returns the previous answer.
+   */
+  const changeQuestion = (nextIndex) => {
+    // Return if out of bounds.
+    if (nextIndex < 0 || nextIndex > questions.length) return
+
+    // Set answer state to its previous state for that question.
+    setAnswer(answers.get(questions[nextIndex].id))
+
+    // Change question view.
+    setCurrentQuestionIndex(nextIndex)
   }
 
   return (
     <div className='Form'>
       <ProgressBar progress={progress} />
       <Question
-        question={questions[currentQuestion].question}
-        icon={questions[currentQuestion].icon}
-        minValue={questions[currentQuestion].minValue}
-        maxValue={questions[currentQuestion].maxValue}
+        question={questions[currentQuestionIndex].question}
+        icon={questions[currentQuestionIndex].icon}
+        minValue={questions[currentQuestionIndex].minValue}
+        maxValue={questions[currentQuestionIndex].maxValue}
+        answer={answer}
+        onChange={onChange}
       />
 
       <div className='FormNav'>
